b1c86c5810cecb673c42c03bf8c413dd66f69f1a
Jeff Earnest <jearnest88@gmail.com>
2016-07-29 22:01:02 -0400

    added Blog

---
+++ blogs/main/apps/blogs/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Blog, Comment
+def index(request):
+    context = {
+    "blogs": Blog.objects.all()
+    # select * from Blog
+    }
+    return render(request, "blogs/index.html", context)
+
+def blogs(request):
+    #using ORM
+    Blog.objects.create(title=request.POST['title'], blog=request.POST['blog'])
+    return redirect('/')
+
+def comments(request, id):
+    blog = Blog.objects.get(id=id)
+    Comment.objects.create(comment=request.POST['comment'], blog = blog)
+    return redirect('/')
+# Create your views here.
